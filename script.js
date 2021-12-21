6036f14d5434fb1b92ddb80d0590288ef1e6e27e
Sylvie-B <unsolo@hotmail.fr>
2021-12-21 14:40:41 +0100

    finish

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $('.hole') // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
+const scoreBoard = $('.score') //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
+const moles = $('.mole') // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+
+// start
+$('#startGame').click(function (){
+    scoreBoard.text(0);
+    let i = 0;
+    let gameTime = setInterval(function (){
+        if(i < 10){
+            let t = Math.floor(Math.random() * 800 + 200);
+            let item = Math.floor(Math.random() * 7);
+            setTimeout(function (){
+                holes.eq(item).addClass('up');
+
+            }, t)
+            holes.removeClass('up');
+            i++;
+        }
+        else {
+            holes.removeClass('up');
+            clearInterval(gameTime);
+        }
+    }, 1000)
+})
+
+moles.click(function (){
+    scoreBoard.text(parseInt(scoreBoard.text()) + 1);
+    $(this).parent().removeClass('up');
+})
